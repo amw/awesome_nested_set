575edbc95f8ae33e087c1dbab6d016f0c3a8d2d7
Adam Wróbel <adam@fluxinc.ca>
2011-03-20 02:19:51 +0100

    Typos.

--- lib/awesome_nested_set/descendants.rb
+++ lib/awesome_nested_set/descendants.rb
@@ -7,15 +7,15 @@ module CollectiveIdea #:nodoc:
         def descendants_count
           return (right - left - 1)/2
         end
-        
+
         def has_descendants?
           !descendants_count.zero?
         end
-        
-        def move_by_direction(ditection)
-          return if ditection.blank?
-          
-          case ditection.to_sym
+
+        def move_by_direction(direction)
+          return if direction.blank?
+
+          case direction.to_sym
             when :up, :left then move_left
             when :down, :right then move_right
           end
